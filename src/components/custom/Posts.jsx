b7bf75653f9451a1e007558db64b6972cf4b1f72
Aziz <muhaariq@gmail.com>
2025-02-05 19:31:00 +0700

    fix: disable cache for nextjs

--- src/components/custom/Posts.jsx
+++ src/components/custom/Posts.jsx
@@ -4,7 +4,9 @@ import Link from 'next/link';
 import React from 'react';
 
 export const Posts = async () => {
-  const res = await fetch(process.env.NEXT_PUBLIC_API_URL);
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL, {
+    next: { revalidate: 0 },
+  });
   const { data } = await res.json();
 
   if (data.length === 0) {
